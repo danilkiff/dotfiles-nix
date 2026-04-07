1478b4f55f89f7d7a7b93a0447d36ad6a9929d08
Oleg Y. Danilkiff <13948753+danilkiff@users.noreply.github.com>
2026-04-07 23:10:16 +0300

    Simplify flake.nix

--- flake.nix
+++ flake.nix
@@ -16,37 +16,26 @@
     }:
     let
       system = "x86_64-linux";
-
-      pkgs = import nixpkgs {
-        inherit system;
-        inherit overlays;
-        config.allowUnfree = true;
-      };
-
+      pkgs = nixpkgs.legacyPackages.${system};
       overlays = [
         (_: prev: {
           sf-pro-fonts = prev.callPackage ./nixos/pkgs/sf-pro-fonts.nix { };
         })
       ];
-
-      mkHost =
-        hostPath:
-        nixpkgs.lib.nixosSystem {
-          modules = [
-            {
-              nixpkgs = {
-                inherit overlays;
-                config.allowUnfree = true;
-              };
-            }
-            home-manager.nixosModules.home-manager
-            hostPath
-          ];
-        };
     in
     {
-      nixosConfigurations = {
-        llathasa = mkHost ./nixos/hosts/llathasa/host.nix;
+      nixosConfigurations.llathasa = nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [
+          {
+            nixpkgs = {
+              inherit overlays;
+              config.allowUnfree = true;
+            };
+          }
+          home-manager.nixosModules.home-manager
+          ./nixos/hosts/llathasa/host.nix
+        ];
       };
 
       devShells.${system}.default = pkgs.mkShell {
@@ -61,7 +50,6 @@
       formatter.${system} = pkgs.nixfmt-rfc-style;
 
       checks.${system} = {
-
         fmt = pkgs.runCommand "fmt-check" { } ''
           ${pkgs.nixfmt-rfc-style}/bin/nixfmt --check ${self}
           mkdir -p $out
