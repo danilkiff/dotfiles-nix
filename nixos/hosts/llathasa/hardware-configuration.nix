5a2e72bfd9fda4281d9ee2c588a2eed469913043
Oleg Y. Danilkiff <13948753+danilkiff@users.noreply.github.com>
2026-04-07 22:56:25 +0300

    Add llathasa host

---
+++ nixos/hosts/llathasa/hardware-configuration.nix
@@ -0,0 +1,54 @@
+# TODO: replace this stub with the output of:
+#   sudo nixos-generate-config --show-hardware-config
+# Current values are placeholders so the flake evaluates in CI;
+# this configuration will NOT boot as-is.
+{
+  config,
+  lib,
+  modulesPath,
+  ...
+}:
+
+{
+  imports = [
+    (modulesPath + "/installer/scan/not-detected.nix")
+  ];
+
+  boot = {
+    initrd = {
+      availableKernelModules = [
+        "nvme"
+        "xhci_pci"
+        "thunderbolt"
+        "usb_storage"
+        "sd_mod"
+      ];
+      kernelModules = [ ];
+    };
+    kernelModules = [ "kvm-intel" ];
+    extraModulePackages = [ ];
+  };
+
+  fileSystems = {
+    "/" = {
+      device = "/dev/disk/by-label/PLACEHOLDER-ROOT";
+      fsType = "ext4";
+    };
+
+    "/boot" = {
+      device = "/dev/disk/by-label/PLACEHOLDER-BOOT";
+      fsType = "vfat";
+      options = [
+        "fmask=0077"
+        "dmask=0077"
+      ];
+    };
+  };
+
+  swapDevices = [ ];
+
+  networking.useDHCP = lib.mkDefault true;
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+}
---
+++ nixos/hosts/llathasa/host.nix
@@ -0,0 +1,19 @@
+_: {
+  imports = [
+    ./hardware-configuration.nix
+    ../../common.nix
+    ../../modules/desktop/fonts.nix
+    ../../modules/desktop/steam.nix
+    ../../modules/desktop/xfce.nix
+  ];
+
+  hardware.graphics.enable = true;
+
+  networking = {
+    hostName = "llathasa";
+    hostId = "af714156"; # head -c 8 /etc/machine-id
+  };
+
+  system.stateVersion = "25.05";
+  home-manager.users.pikachu.home.stateVersion = "25.05";
+}
